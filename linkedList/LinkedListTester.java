f98c49dd99c28ff87814944fbed02ea4acd77d44
kevinjt2001 <kjtorrealba@gmail.com>
2025-02-12 14:05:48 -0500

    Added some testing code for the LinkedList, tester methods still need to be coded

--- linkedList/LinkedListTester.java
+++ linkedList/LinkedListTester.java
@@ -4,11 +4,34 @@ public class LinkedListTester {
 
 	public static void main(String[] args) {
 		// create implementation, then...
-		
-		LinkedList tester = new LinkedListImpl();
-		tester.addItem("an item to add");
-		
 
+		LinkedList tester = new LinkedListImpl();
+		tester.addItem("First");
+		tester.addItem("Second");
+		tester.addItem("Third");
+
+		Integer numItems = tester.itemCount();
+		System.out.println(numItems);
+
+		boolean exists = tester.isItemInList("Fourth");
+
+		if (exists) {
+			System.out.println("Item exists");
+		} else {
+			System.out.println("Item does not exist");
+		}
+
+		tester.listItems();
+
+		boolean deleted = tester.deleteItem("First");
+
+		if (deleted) {
+			System.out.println("Success");
+		} else {
+			System.out.println("Fail");
+		}
+
+		tester.listItems();
 	}
 
 }
