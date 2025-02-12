42eac233e7f6465a44a3ecf8fd37f103716cd7ca
kevinjt2001 <kjtorrealba@gmail.com>
2025-02-12 14:04:48 -0500

    Completed 5 methods (isItemInList, addItem, itemCount, listItem, & deleteItem).

--- linkedList/LinkedListImpl.java
+++ linkedList/LinkedListImpl.java
@@ -5,49 +5,95 @@ public class LinkedListImpl implements LinkedList {
 
 	@Override
 	public Boolean isItemInList(String thisItem) {
-		// TODO Auto-generated method stub
-		return null;
+		if (head == null) {
+			return false;
+		}
+
+		ListItem current = head;
+		while (current != null) {
+			if (current.data.equals(thisItem)) {
+				return true;
+			}
+			current = current.next;
+		}
+
+		return false;
 	}
 
 	@Override
 	public Boolean addItem(String thisItem) {
-				
-				
-				if(head == null) {
-					head = new ListItem(thisItem);
-					return true;
-				}
-				
-				ListItem current = head;
-				while(current.next != null) {
-					current = current.next;
-				}
-				current.next = new ListItem(thisItem);
-				
-				System.out.println("Hello World");				
-				
-		
+		if (thisItem == null) {
+			return false;
+		}
+
+		if (head == null) {
+			head = new ListItem(thisItem);
+			System.out.println("hello from addItem in LinkedListImpl - the item passed in: " + thisItem);
+			return true;
+		}
+
+		ListItem current = head;
+		while (current != null) {
+			if (current.data.equals(thisItem)) {
+				return false;
+			}
+			if (current.next == null) {
+				break;
+			}
+			current = current.next;
+		}
+
+		current.next = new ListItem(thisItem);
+
 		System.out.println("hello from addItem in LinkedListImpl - the item passed in: " + thisItem);
-	
-		return null;
+
+		return true;
+
 	}
 
 	@Override
 	public Integer itemCount() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer count = 0;
+		ListItem temp = head;
+		while (temp != null) {
+			count++;
+			temp = temp.next;
+		}
+		return count;
 	}
 
 	@Override
 	public void listItems() {
-		// TODO Auto-generated method stub
-		
+		ListItem current = head;
+		while (current != null) {
+			System.out.println(current.data);
+			current = current.next;
+		}
 	}
 
 	@Override
 	public Boolean deleteItem(String thisItem) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (head == null) {
+			return false;
+		}
+
+		if (head.data.equals(thisItem)) {
+			head = head.next;
+			return true;
+		}
+
+		ListItem current = head;
+		while (current.next != null) {
+			if (current.next.data.equals(thisItem)) {
+				current.next = current.next.next;
+				return true;
+			}
+			current = current.next;
+
+		}
+
+		return false;
 	}
 
 	@Override
@@ -65,7 +111,7 @@ public class LinkedListImpl implements LinkedList {
 	@Override
 	public void sort() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
