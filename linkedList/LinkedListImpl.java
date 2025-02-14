5988c8f72eda6b78b72db68ac177722bde4301b7
kevinjt2001 <kjtorrealba@gmail.com>
2025-02-14 00:19:17 -0500

    Reworked & finished testing, changes to implementation

--- linkedList/LinkedListImpl.java
+++ linkedList/LinkedListImpl.java
@@ -6,26 +6,27 @@ public class LinkedListImpl implements LinkedList {
 	@Override
 	public Boolean isItemInList(String thisItem) {
 		if (head == null) {
-			System.out.printf("Item '%s' does not exist in the LinkedList.\n", thisItem);
+			System.out.printf("Item '%s' does not exist in the List\n", thisItem);
 			return false;
 		}
 
 		ListItem current = head;
 		while (current != null) {
 			if (current.data.equals(thisItem)) {
-				System.out.printf("Item '%s' exists in the LinkedList.\n", thisItem);
+				System.out.printf("Item '%s' exists in the List\n", thisItem);
 				return true;
 			}
 			current = current.next;
 		}
 
-		System.out.printf("Item '%s' does not exist in the LinkedList.\n", thisItem);
+		System.out.printf("Item '%s' does not exist in the List\n", thisItem);
 		return false;
 	}
 
 	@Override
 	public Boolean addItem(String thisItem) {
 		if (thisItem == null) {
+			System.err.println("Error adding item");
 			return false;
 		}
 
@@ -62,14 +63,17 @@ public class LinkedListImpl implements LinkedList {
 			count++;
 			temp = temp.next;
 		}
+
+		System.out.printf("%d item(s) in the List\n", count);
 		return count;
 	}
 
 	@Override
 	public void listItems() {
+		System.out.println("Items in List:");
 		ListItem current = head;
 		while (current != null) {
-			System.out.println(current.data);
+			System.out.printf("- %s\n", current.data);
 			current = current.next;
 		}
 	}
@@ -78,11 +82,13 @@ public class LinkedListImpl implements LinkedList {
 	public Boolean deleteItem(String thisItem) {
 
 		if (head == null) {
+			System.out.println("Error deleting item");
 			return false;
 		}
 
 		if (head.data.equals(thisItem)) {
 			head = head.next;
+			System.out.println("Item deleted: " + thisItem);
 			return true;
 		}
 
@@ -90,12 +96,14 @@ public class LinkedListImpl implements LinkedList {
 		while (current.next != null) {
 			if (current.next.data.equals(thisItem)) {
 				current.next = current.next.next;
+				System.out.println("Item deleted: " + thisItem);
 				return true;
 			}
 			current = current.next;
 
 		}
 
+		System.out.printf("Item '%s' could not be deleted\n", thisItem);
 		return false;
 	}
 
@@ -103,6 +111,7 @@ public class LinkedListImpl implements LinkedList {
 	public Boolean insertBefore(String newItem, String itemToInsertBefore) {
 
 		if (head == null) {
+			System.out.println("Item not inserted, List is empty");
 			return false;
 		}
 
@@ -113,12 +122,14 @@ public class LinkedListImpl implements LinkedList {
 				ListItem newNode = new ListItem(newItem);
 				newNode.next = prevNode.next;
 				prevNode.next = newNode;
+				System.out.println(String.format("Item '%s' inserted before '%s'", newItem, itemToInsertBefore));
 				return true;
 			}
 
 			current = current.next;
 		}
 
+		System.out.printf("Item '%s' could not be inserted\n", newItem);
 		return false;
 	}
 
@@ -126,6 +137,7 @@ public class LinkedListImpl implements LinkedList {
 	public Boolean insertAfter(String newItem, String itemToInsertAfter) {
 
 		if (head == null) {
+			System.out.println("Item not inserted, List is empty");
 			return false;
 		}
 
@@ -135,12 +147,14 @@ public class LinkedListImpl implements LinkedList {
 				ListItem newNode = new ListItem(newItem);
 				newNode.next = current.next;
 				current.next = newNode;
+				System.out.println(String.format("Item '%s' inserted after '%s'", newItem, itemToInsertAfter));
 				return true;
 			}
 
 			current = current.next;
 		}
 
+		System.out.printf("Item '%s' could not be inserted\n", newItem);
 		return false;
 	}
 
--- linkedList/LinkedListTester.java
+++ linkedList/LinkedListTester.java
@@ -5,55 +5,29 @@ public class LinkedListTester {
 	private static LinkedList tester = new LinkedListImpl();
 
 	public static void main(String[] args) {
+		runLinkedListTests();
 
-		Integer numItems = tester.itemCount();
-		System.out.println(numItems);
-
-		boolean exists = tester.isItemInList("Fourth");
-
-		if (exists) {
-			System.out.println("Item exists");
-		} else {
-			System.out.println("Item does not exist");
-		}
-
-		tester.listItems();
-
-		boolean deleted = tester.deleteItem("First");
-
-		tester.listItems();
-
-		boolean added = tester.insertBefore("TEST", "Third");
-
-		tester.listItems();
 	}
 
 	public static void runLinkedListTests() {
-		TEST_addItem();
-
-		TEST_itemCount();
-
-		TEST_isItemInList();
-
-	}
-
-	public static void TEST_addItem() {
 		tester.addItem("First");
 		tester.addItem("Second");
 		tester.addItem("Third");
 
-	}
+		tester.itemCount();
 
-	public static void TEST_itemCount() {
-		Integer count = tester.itemCount();
-		System.out.printf("%d item(s) in the LinkedList.\n", count);
-	}
-
-	public static void TEST_isItemInList() {
 		tester.isItemInList("Fourth");
-	}
 
-	public static void TEST_deleteItem() {
+		tester.listItems();
+
+		tester.deleteItem("Second");
+
+		tester.insertAfter("Fifth", "Third");
+
+		tester.insertBefore("Fourth", "Fifth");
+
+		tester.itemCount();
+		tester.listItems();
 
 	}
 
